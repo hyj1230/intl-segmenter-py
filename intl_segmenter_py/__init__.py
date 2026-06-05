1ab1967c32243b18975846d064f63fe87c507b48
hyj1230 <121798333+hyj1230@users.noreply.github.com>
2026-06-06 02:34:05 +0800

    更新 __init__.py

--- intl_segmenter_py/__init__.py
+++ intl_segmenter_py/__init__.py
@@ -1,4 +1,4 @@
-from .general import isAlphabetic, isAlphanumeric, isLetter, isNumeric
+from .general import isAlphabetic, isAlphanumeric, isLetter, isNumeric, isMark
 from .emoji import isEmoji, isEmojiPresentation, isExtendedPictographic
 from .grapheme import graphemeSegments, countGraphemes, splitGraphemes, isIndicConjunctConsonant, cat
 from .word import ascii_word_bounds, unicode_word_bounds, word_bounds, is_word_like
