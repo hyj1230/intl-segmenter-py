625bdf8a9dd5af58013f4df96efa0010bcb9d556
hyj1230 <121798333+hyj1230@users.noreply.github.com>
2026-06-06 03:25:25 +0800

    更新 __init__.py

--- intl_segmenter_py/__init__.py
+++ intl_segmenter_py/__init__.py
@@ -1,5 +1,6 @@
 from .general import isAlphabetic, isAlphanumeric, isLetter, isNumeric, isMark, isPunctuation, isSymbol, isCo, isNd
 from .emoji import isEmoji, isEmojiPresentation, isExtendedPictographic
+from .script import isArabic
 from .grapheme import graphemeSegments, countGraphemes, splitGraphemes, isIndicConjunctConsonant, cat
 from .word import ascii_word_bounds, unicode_word_bounds, word_bounds, is_word_like
 from .compat_util import String
